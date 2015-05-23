d791c65802b9443413b455e2208dda59bac2d453
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-23 13:05:35 +0000

    js files only for homepage

--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -1,3 +1,7 @@
+<?php
+if($page==="Homepage")
+{
+?>
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
 <script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
@@ -7,3 +11,6 @@
 <script type="text/javascript" src="/cadeui/system/js/scrollflow.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/sticky.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
+<?php
+}
+?>
