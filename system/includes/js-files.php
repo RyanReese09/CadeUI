541cbe874d03d0e7adb1df2050bdc6aed96a7e5d
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-23 13:07:30 +0000

    added back page specific js

--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -1,16 +1,21 @@
 <?php
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
 if($page==="Homepage")
 {
 ?>
-<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
-<script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/cadeui/system/js/login.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollit.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollflow.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/sticky.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
+<?php
+}
+else if($page==="Login")
+{
+?>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+<script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+<script type="text/javascript" src="/cadeui/system/js/login.js"></script>
 <?php
 }
 ?>
