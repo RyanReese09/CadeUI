7e163865e79a8ae4bc8d667a1f846f1238b78296
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-23 13:10:29 +0000

    jqueryui was on login page and not homepage

--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -3,6 +3,7 @@
 if($page==="Homepage")
 {
 ?>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollit.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollflow.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/sticky.js"></script>
@@ -12,7 +13,6 @@ if($page==="Homepage")
 else if($page==="Login")
 {
 ?>
-<script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
 <script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/login.js"></script>
