f398d25cc3e22200012b195d6ef5902f91bf69ca
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-23 12:56:37 +0000

    added jscript files to homepage

--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -11,6 +11,9 @@ if($page==="Login")
 else if($page==="Homepage")
 {
 ?>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+<script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+<script type="text/javascript" src="/cadeui/system/js/login.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollit.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollflow.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/sticky.js"></script>
