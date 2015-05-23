50b895c8b60a546bf8f84f234cb2321e93047710
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-23 13:00:40 +0000

    moved around include js files

--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -13,18 +13,12 @@ else if($page==="Homepage")
 ?>
 <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
 <script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/login.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollit.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollflow.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/sticky.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
-<?php
-}
-else
-{
-?>
-<script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
 <?php
 }
 ?>
+<script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
