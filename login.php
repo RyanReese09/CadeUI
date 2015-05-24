ab6941a81b2f8e861cd64610771e8f4495c7f65c
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-24 13:05:35 +0000

    login.php should now be ajax based

--- login.php
+++ login.php
@@ -1,42 +1,11 @@
 <?php
 $page="Login";
-if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
-{
-?>
-<div class="login-wrapper">
-  <form method="post" action="/cadeui/system/includes/process-login" id="login" name="login">
-    <fieldset>
-      <?php
-      if(isset($_GET["error"]) && $_GET["error"]==="userpass")
-        echo "<span id=\"userpass-error\" class=\"error\">Error: Your username or password is incorrect. Please try again.</span>\n";
-      else if(isset($_GET["error"]) && $_GET["error"]==="attempts")
-        echo "<span id=\"attempts-error\" class=\"error\">Error: You have exceeded the maximum number of attempts. Please try again later.</span>\n";
-      ?>
-<label for="email">Username</label>
-      <input type="email" class="envelope" placeholder="Username" id="email" name="email">
-      <div class="password-holder">
-        <label for="password">Password</label>
-        <span class="showHide showPW remove">Show</span>
-      </div>
-      <input type="password" class="lock" placeholder="Password" id="password" name="password">
-      <div class="submit-holder">
-        <div class="rem-pw-holder">
-          <label class="remember-me"><input type="checkbox" name="remember" id="remember" value="yes">Remember Me</label>
-          <a href="#" class="forgotpw">I forgot my password...</a>
-        </div>
-        <input type="submit" id="submit" value="Login">
-      </div>
-     </fieldset>
-  </form>
-</div>
-<?php
-require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/js-files.php");
-}
-else
+if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest')
 {
 require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/header.php");
+echo "<body>";
+}
 ?>
-<body>
 <div class="login-wrapper">
   <form method="post" action="/cadeui/system/includes/process-login" id="login" name="login">
     <fieldset>
@@ -65,6 +34,8 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/header.php");
 </div>
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/js-files.php");
+if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest')
+{
 ?>
 </body>
 </html>
--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -12,9 +12,18 @@ if($page==="Homepage")
 <script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
 <?php
 }
+else if($page==="Login" && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+{
+?>
+<script type="text/javascript" src="/cadeui/system/js/login.js"></script>
+<?php
+}
 else if($page==="Login")
 {
 ?>
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+<script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/login.js"></script>
 <?php
 }
