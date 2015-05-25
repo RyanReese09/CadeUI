ac37b56d1d52564a4746db662f8ee20f4ef4c8a0
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-25 02:43:46 +0000

    made protocol agnostic

--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -2,10 +2,10 @@
 if($page==="Homepage")
 {
 ?>
-<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
-<script type="text/javascript" src="http://jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+<script type="text/javascript" src="//jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollit.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scrollflow.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/sticky.js"></script>
