4f633ca3b8d285598bb1b66f2544e9d8bc114181
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-25 03:14:33 +0000

    base functionality of dashboard showing content to users not logged in

--- dashboard.php
+++ dashboard.php
@@ -4,20 +4,25 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/header.php");
 ?>
 <body>
 <?php
-echo "Hello ".htmlspecialchars($_SESSION["firstname"])." ".htmlspecialchars($_SESSION["lastname"]);
+if(isset($_SESSION["loggedin"]))
+{
+  echo "Hello ".htmlspecialchars($_SESSION["firstname"])." ".htmlspecialchars($_SESSION["lastname"]);
 
-echo "<br><br>Username is: ".htmlspecialchars($_SESSION["username"]);
+  echo "<br><br>Username is: ".htmlspecialchars($_SESSION["username"]);
 
-echo "<br><br>ID is: ".htmlspecialchars($_SESSION["userID"]);
+  echo "<br><br>ID is: ".htmlspecialchars($_SESSION["userID"]);
 
-echo "<br><br>Your join date is: ".htmlspecialchars($_SESSION["joindate"]);
+  echo "<br><br>Your join date is: ".htmlspecialchars($_SESSION["joindate"]);
 
-if(!empty($_COOKIE["rememberMe"]))
-echo "<br><br>You chose us to remember you. How cute.";
-
+  if(!empty($_COOKIE["rememberMe"]))
+    echo "<br><br>You chose us to remember you. How cute.";
 ?>
 <br><br><a href="/cadeui/logout" title="Logout" class="logout">Logout</a>
 <?php
+}
+else
+  echo "Not logged in...";
+  
 require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/js-files.php");
 ?>
 </body>
