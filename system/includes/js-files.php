961234de4588a6f2e419ac51a5124f3c77b4ef1f
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-27 20:47:49 +0000

    renamed login

--- system/includes/js-files.php
+++ system/includes/js-files.php
@@ -10,13 +10,12 @@ if($page==="Homepage")
 <script type="text/javascript" src="/cadeui/system/js/scrollflow.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/sticky.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
-<script type="text/javascript" src="/cadeui/system/js/subscribers.js"></script>
 <?php
 }
 else if($page==="Login" && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
 {
 ?>
-<script type="text/javascript" src="/cadeui/system/js/login.js"></script>
+<script type="text/javascript" src="/cadeui/system/js/process-home.js"></script>
 <?php
 }
 else if($page==="Login")
@@ -25,7 +24,7 @@ else if($page==="Login")
 <script type="text/javascript" src="//code.jquery.com/jquery-2.1.3.min.js"></script>
 <script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
 <script type="text/javascript" src="//jqueryvalidation.org/files/dist/additional-methods.min.js"></script>
-<script type="text/javascript" src="/cadeui/system/js/login.js"></script>
+<script type="text/javascript" src="/cadeui/system/js/process-home.js"></script>
 <script type="text/javascript" src="/cadeui/system/js/scripts.js"></script>
 <?php
 }
