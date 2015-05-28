0ec10dccb29b4e44b7c60eb0cbbc91965ebd737e
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-28 20:39:53 +0000

    switched up JS to make it more future-proof

--- system/js/scripts.js
+++ system/js/scripts.js
@@ -30,29 +30,3 @@ $(".login").click(function(e){
     width: $(window).width()
   });
 });
-
-$(".top-nav ul li a").click(function(){
-  setTimeout(function(){
-    $(".hamburger").attr("checked", false);
-  }, 300);
-});
-//Sticky menu. Non-hamburger menu
-$(".top-nav").sticky({topSpacing:0});
-
-var navH=-($(".top-nav").height());
-$.scrollIt({
-  topOffset: navH
-});
-$("#subscribers").validate({
-  errorClass: "error",
-  errorElement: "span",
-  rules: {
-    subEmail: {
-      required: true,
-      email: true
-    }
-  },
-  messages: {
-    subEmail: "Error: Please enter a valid e-mail address"
-  }
-});
