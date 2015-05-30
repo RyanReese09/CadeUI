45f9f77b728669669111091374b626ef4d2fdc15
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-30 00:49:55 +0000

    success message

--- system/js/home.js
+++ system/js/home.js
@@ -46,7 +46,7 @@ $("#subscribers").submit(function(event) {
         {
           $(".overlay").remove();
           $("#subscribers").fadeTo(1000, 0);
-          $("<p class=\"success-message\">Congrats</p>").appendTo("#newsletter").delay(1500).fadeIn(1000);
+          $("<p class=\"success-message\">Success: You have been successfully subscribed to the CadeUI newsletter</p>").appendTo("#newsletter").delay(1500).fadeIn(1000);
         }
         else
         {
