3d345ac1e1534524edb217defa16d7d0713beff6
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-30 19:07:09 +0000

    using gmail smtp

--- system/includes/classes/Email.class.php
+++ system/includes/classes/Email.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once("/cadeui/system/includes/PHPMailer/PHPMailerAutoload.php");
+require "../PHPMailer/PHPMailerAutoload.php";
 class Email
 {
   public function __construct($address, $authKey)
@@ -12,12 +12,12 @@ class Email
     $mail = new PHPMailer;
 
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = "mail.codefundamentals.com";  // Specify main and backup SMTP servers
+    $mail->Host = "smtp.gmail.com";  // Specify main and backup SMTP servers. Try localhost if this fails -RR
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = "admin@codefundamentals.com";                 // SMTP username
     $mail->Password = "RubiksCubes09!";
     $mail->SMTPSecure = 'tls';
-    $mail->Port = 25;
+    $mail->Port = 587;
     $mail->From = 'admin@codefundamentals.com';
     $mail->FromName = 'Mailer';
     $mail->addAddress('sportsdude.reese@gmail.com', 'Joe User');
