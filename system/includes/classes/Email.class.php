7e10a5f06f6884c6238984aefc3ff86f80f16ced
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-30 18:39:27 +0000

    trying php mailer

--- system/includes/classes/Email.class.php
+++ system/includes/classes/Email.class.php
@@ -8,12 +8,38 @@ class Email
   }
   public function subscriber()
   {
-    $test=mail("admin@codefundamentals.com", "subject", "body");
-    if($test)
-      return true;
+    require_once("/cadeui/system/includes/PHPMailer/PHPMailerAutoload.php");
+
+    $mail = new PHPMailer;
+
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = "localhost";  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = "admin@codefundamentals.com";                 // SMTP username
+    $mail->Password = "RubiksCubes09!";                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    $mail->From = 'admin@codefundamentals.com';
+    $mail->FromName = 'Mailer';
+    $mail->addAddress('sportsdude.reese@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addReplyTo('admin@codefundamentals.com', 'Information');
+
+    $mail->isHTML(true);                                  // Set email format to HTML
+
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if(!$mail->send())
+    {
+      echo 'Message could not be sent.';
+      echo 'Mailer Error: ' . $mail->ErrorInfo;
+    }
     else
-      return false;
-    
+    {
+      echo 'Message has been sent';
+    }
   }
 }
 ?>
