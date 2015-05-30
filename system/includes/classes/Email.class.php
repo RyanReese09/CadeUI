068fbeedd1af67a8cbc54673f40c377042d84fe4
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-30 16:42:18 +0000

    removed bbc

--- system/includes/classes/Email.class.php
+++ system/includes/classes/Email.class.php
@@ -6,7 +6,6 @@ class Email
     $this->address=$address;
     $this->authKey=$authKey;
     $headers = 'From: admin@codefundamentals.com' . "\r\n" ;
-    $headers .= 'BCC: ' . implode(', ', $recipients) . "\r\n";
     $headers .= 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     mail("sportsdude.reese@gmail.com", "subject", "body", $headers);
