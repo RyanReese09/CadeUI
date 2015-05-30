db13e513a59a0ce83bb5b146835769fcda42a1e2
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-30 18:50:20 +0000

    removed error

--- system/includes/classes/Email.class.php
+++ system/includes/classes/Email.class.php
@@ -16,28 +16,25 @@ class Email
     $mail->Host = "localhost";  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = "admin@codefundamentals.com";                 // SMTP username
-    $mail->Password = "RubiksCubes09!";                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 25;                                    // TCP port to connect to
-
+    $mail->Password = "RubiksCubes09!";
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 25;
     $mail->From = 'admin@codefundamentals.com';
     $mail->FromName = 'Mailer';
-    $mail->addAddress('sportsdude.reese@gmail.com', 'Joe User');     // Add a recipient
-    $mail->addReplyTo('admin@codefundamentals.com', 'Information');
-
-    $mail->isHTML(true);                                  // Set email format to HTML
-
+    $mail->addAddress('sportsdude.reese@gmail.com', 'Joe User');
+    $mail->isHTML(true);
     $mail->Subject = 'Here is the subject';
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     if(!$mail->send())
     {
-      return false$mail->ErrorInfo;
+      return false;
+      //$mail->ErrorInfo;
     }
     else
     {
-      echo 'Message has been sent';
+      return true;
     }
   }
 }
