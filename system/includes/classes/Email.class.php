0308e55b248cbe200d4807b6f9d0ad5083351d8b
ryanreese09 <sportsdude.reese@gmail.com>
2015-05-30 17:49:24 +0000

    test mail

--- system/includes/classes/Email.class.php
+++ system/includes/classes/Email.class.php
@@ -8,10 +8,7 @@ class Email
   }
   public function subscriber()
   {
-    $headers = 'From: admin@codefundamentals.com' . "\r\n" ;
-    $headers .= 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $test=mail("sportsdude.reese@gmail.com", "subject", "body", $headers);
+    $test=mail("sportsdude.reese@gmail.com", "subject", "body");
     if($test)
       return true;
     else
