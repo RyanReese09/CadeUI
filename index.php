43fb3c7acf7747ea3f27527c99b361dbfb09d6e5
ryanreese09 <sportsdude.reese@gmail.com>
2015-06-01 21:15:16 +0000

    moving header

--- index.php
+++ index.php
@@ -3,37 +3,9 @@ $page="Homepage";
 require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/header.php");
 ?>
 <body>
-<header id="top" data-scroll-index="0" role="banner">
-  <div class="header-inner clearfix">
-    <a href="/cadeui/" class="logo" title="Home" tabindex="1"><img src="/cadeui/system/images/cadeui-logo.png" width="300" height="100" alt="CadeUI"></a>
-    <?php
-    if(isset($_SESSION["loggedin"]))
-    {
-    ?>
-    <a href="/cadeui/logout" class="logout user-links" title="Logout" tabindex="2">Logout</a>
-    <?php
-    }
-    else
-    {
-    ?>
-    <a href="/cadeui/register" class="register user-links" title="Register" role="button" tabindex="2">Register</a> |
-    <a href="/cadeui/login" class="login user-links" title="Login" role="button" tabindex="3">Login</a>
-    <?php
-    }
-    ?>
-  </div>
-  <nav class="top-nav" role="navigation">
-    <input type="checkbox" class="hamburger" id="hamburger">
-    <label class="trigger" for="hamburger" tabindex="4"><span></span></label>
-    <ul role="menu">
-      <li><a href="#top" data-scroll-nav="0" title="Home" tabindex="5">Home</a></li>
-      <li><a href="#getting-started" data-scroll-nav="1" title="Getting Started" tabindex="6">Get Started</a></li>
-      <li><a href="#features" data-scroll-nav="2" title="CadeUI Features" tabindex="7">Features</a></li>
-      <li><a href="#testimonials" data-scroll-nav="3" title="Testimonials" tabindex="8">Testimonials</a></li>
-      <li><a href="#newsletter" data-scroll-nav="4" title="Newsletter Signup" tabindex="9">Newsletter</a></li>
-    </ul>
-  </nav>
-</header>
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/cadeui/system/includes/page-header.php");
+?>
 <div class="banner-why" role="contentinfo" aria-labelledby="whatiscadeui" aria-describedby="description">
   <div class="banner-inner">
     <div class="banner-text">
