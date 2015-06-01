f1524e71a67a73a21877a4acb71e52b3a07f6f63
ryanreese09 <sportsdude.reese@gmail.com>
2015-06-01 21:20:54 +0000

    source formatting

--- system/includes/page-header.php
+++ system/includes/page-header.php
@@ -1,25 +1,14 @@
 <header id="top" data-scroll-index="0" role="banner">
   <div class="header-inner clearfix">
     <a href="/cadeui/" class="logo" title="Home" tabindex="1"><img src="/cadeui/system/images/cadeui-logo.png" width="300" height="100" alt="CadeUI"></a>
-    <?php
-    if(isset($_SESSION["loggedin"]))
-    {
-    ?>
+    <?php if(isset($_SESSION["loggedin"])) { ?>
     <a href="/cadeui/logout" class="logout user-links" title="Logout" tabindex="2">Logout</a>
-    <?php
-    }
-    else
-    {
-    ?>
-<a href="/cadeui/register" class="register user-links" title="Register" role="button" tabindex="2">Register</a> |
+    <?php } else { ?>
+    <a href="/cadeui/register" class="register user-links" title="Register" role="button" tabindex="2">Register</a> |
     <a href="/cadeui/login" class="login user-links" title="Login" role="button" tabindex="3">Login</a>
-    <?php
-    }
-    ?></div>
-  <?php
-  if($page==="Homepage")
-  {
-  ?>
+    <?php } ?>
+  </div>
+  <?php if($page==="Homepage") { ?>
   <nav class="top-nav" role="navigation">
     <input type="checkbox" class="hamburger" id="hamburger">
     <label class="trigger" for="hamburger" tabindex="4"><span></span></label>
@@ -31,7 +20,5 @@
       <li><a href="#newsletter" data-scroll-nav="4" title="Newsletter Signup" tabindex="9">Newsletter</a></li>
     </ul>
   </nav>
-  <?php
-  }
-  ?>
+  <?php } ?>
 </header>
